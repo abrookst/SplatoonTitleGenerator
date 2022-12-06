2143465b7c3a0e30c18b5446a991335d5c0b2bc6
Ali Brooks <36117420+abrookst@users.noreply.github.com>
2022-12-06 13:53:22 -0500

    updates

--- titlecard.js
+++ titlecard.js
@@ -1,7 +1,24 @@
 function refreshImage(){
+    
     var canvas = document.getElementById("TCGN");
     var ctx = canvas.getContext("2d");
-    ctx.font = "30px Arial";
+    
+    ctx.strokeStyle = "#181818";
+    ctx.fillStyle = "#181818";
+    ctx.beginPath();
+    ctx.roundRect(10, 10, 200, 200, 25);
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.roundRect(220, 10, 170, 40, 50);
+    ctx.stroke();
+    ctx.fill();
+    
+    ctx.font = "50px";
+    ctx.strokeStyle = "rgb(137, 250, 53)";
+    ctx.strokeText("Level ", 225, 30);
+
     var Username = document.getElementById('Username');
     ctx.strokeText(Username.value,10,50);
     var SFC = document.getElementById('SFC');
