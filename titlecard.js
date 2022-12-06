16ca423d2b1f1aa594934f08bdedcd01eb012e1b
Ali Brooks <36117420+abrookst@users.noreply.github.com>
2022-12-05 22:36:43 -0500

    next

---
+++ titlecard.js
@@ -0,0 +1,13 @@
+
+
+function refreshImage(){
+    var canvas = document.getElementById("TCGN");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "30px Arial";
+    var Username = document.getElementById('Username');
+    ctx.strokeText(Username.value,10,50);
+    var SFC = document.getElementById('SFC');
+    ctx.strokeText(SFC.value,10,80);
+    var Level = document.getElementById('Level');
+    ctx.strokeText(Level.value,10,110);
+}
